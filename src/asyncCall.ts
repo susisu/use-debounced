6c08b1f634387d12a92ed8539adbdb63de40b474
Susisu <susisu2413@yahoo.co.jp>
2020-04-19 16:52:28 +0900

    fix: use only one state to avoid flickering
    Reverts 238b6834268f29b4129e6641330f6cfd33897f24 and a753530b6520a9d19a3de3fff3bf2bb7d3234746

--- src/asyncCall.ts
+++ src/asyncCall.ts
@@ -1,88 +1,6 @@
-import { useRef, useCallback, useEffect, useState } from "react";
+import { useRef, useCallback, useEffect, useReducer, Reducer } from "react";
 import { attachActions, CancelFunc } from "@susisu/promise-utils";
-import { useDebouncedCall } from "./call";
-
-type UseAsyncCallOptions<R, T extends readonly unknown[]> = Readonly<{
-  func: (...args: T) => Promise<R>;
-  init: R | (() => R);
-}>;
-
-type UseAsyncCallResult<R, T extends readonly unknown[]> = [
-  R, // result
-  (...args: T) => void, // call
-  boolean, // isWaiting
-  {
-    cancel: () => void;
-    reset: (result: R) => void;
-  }
-];
-
-/**
- * Auxiliary hook to implement useDebouncedAsyncCall.
- */
-function useAsyncCall<R, T extends readonly unknown[]>(
-  options: UseAsyncCallOptions<R, T>
-): UseAsyncCallResult<R, T> {
-  const [result, setResult] = useState<R>(options.init);
-  const [isWaiting, setIsWaiting] = useState(false);
-
-  const cancelAsyncCallRef = useRef<CancelFunc | undefined>(undefined);
-
-  const call = useCallback(
-    (...args: T): void => {
-      if (cancelAsyncCallRef.current) {
-        const cancelAsyncCall = cancelAsyncCallRef.current;
-        cancelAsyncCall();
-        cancelAsyncCallRef.current = undefined;
-      }
-      setIsWaiting(true);
-      const func = options.func;
-      [cancelAsyncCallRef.current] = attachActions(
-        func(...args),
-        result => {
-          cancelAsyncCallRef.current = undefined;
-          setResult(result);
-          setIsWaiting(false);
-        },
-        err => {
-          // eslint-disable-next-line no-console
-          console.error(err);
-          cancelAsyncCallRef.current = undefined;
-          setIsWaiting(false);
-        }
-      );
-    },
-    [options.func]
-  );
-
-  const cancelRef = useRef((): void => {
-    if (cancelAsyncCallRef.current) {
-      const cancelAsyncCall = cancelAsyncCallRef.current;
-      cancelAsyncCall();
-      cancelAsyncCallRef.current = undefined;
-    }
-    setIsWaiting(false);
-  });
-
-  const resetRef = useRef((result: R): void => {
-    const cancel = cancelRef.current;
-    cancel();
-    setResult(result);
-  });
-
-  useEffect(
-    () => () => {
-      if (cancelAsyncCallRef.current) {
-        const cancelAsyncCall = cancelAsyncCallRef.current;
-        cancelAsyncCall();
-        cancelAsyncCallRef.current = undefined;
-      }
-    },
-    []
-  );
-
-  return [result, call, isWaiting, { cancel: cancelRef.current, reset: resetRef.current }];
-}
+import { useDebouncedPrim } from "./prim";
 
 export type UseDebouncedAsyncCallOptions<R, T extends readonly unknown[]> = Readonly<{
   func: (...args: T) => Promise<R>;
@@ -104,6 +22,150 @@ export type UseDebouncedAsyncCallResult<R, T extends readonly unknown[]> = [
   }
 ];
 
+type State<R> =
+  | Readonly<{ type: "standby"; result: R }>
+  | Readonly<{ type: "waiting"; result: R }>
+  | Readonly<{ type: "pending"; result: R }>
+  | Readonly<{ type: "waiting-pending"; result: R }>;
+
+type Action<R> =
+  | Readonly<{ type: "start" }>
+  | Readonly<{ type: "leadingCall"; skip: boolean }>
+  | Readonly<{ type: "trailingCall"; skip: boolean }>
+  | Readonly<{ type: "fulfill"; result: R }>
+  | Readonly<{ type: "reject" }>
+  | Readonly<{ type: "cancel" }>
+  | Readonly<{ type: "reset"; result: R }>;
+
+const initializer = <R>(init: R | (() => R)): State<R> => {
+  if (typeof init === "function") {
+    const initFunc = init as () => R;
+    return { type: "standby", result: initFunc() };
+  } else {
+    return { type: "standby", result: init };
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleStart = <R>(state: State<R>): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "standby":
+      return { type: "waiting", result: state.result };
+    case "pending":
+      return { type: "waiting-pending", result: state.result };
+    case "waiting":
+    case "waiting-pending":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleLeadingCall = <R>(state: State<R>, skip: boolean): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "waiting":
+      return skip ? state : { type: "waiting-pending", result: state.result };
+    case "waiting-pending":
+      return state;
+    case "standby":
+    case "pending":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleTrailingCall = <R>(state: State<R>, skip: boolean): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "waiting":
+      return skip
+        ? { type: "standby", result: state.result }
+        : { type: "pending", result: state.result };
+    case "waiting-pending":
+      return { type: "pending", result: state.result };
+    case "standby":
+    case "pending":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleFulfill = <R>(state: State<R>, result: R): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "pending":
+      return { type: "standby", result };
+    case "waiting-pending":
+      return { type: "waiting", result };
+    case "standby":
+    case "waiting":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleReject = <R>(state: State<R>): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "pending":
+      return { type: "standby", result: state.result };
+    case "waiting-pending":
+      return { type: "waiting", result: state.result };
+    case "standby":
+    case "waiting":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleCancel = <R>(state: State<R>): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    case "standby":
+      return state;
+    case "waiting":
+    case "pending":
+    case "waiting-pending":
+      return { type: "standby", result: state.result };
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const handleReset = <R>(state: State<R>, result: R): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (state.type) {
+    // assuming it has already been cancelled
+    case "standby":
+      return state.result === result ? state : { type: "standby", result };
+    case "waiting":
+    case "pending":
+    case "waiting-pending":
+      throw new Error(`unexpected state: ${state.type}`);
+  }
+};
+
+// eslint-disable-next-line consistent-return
+const reducer = <R>(state: State<R>, action: Action<R>): State<R> => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "start":
+      return handleStart(state);
+    case "leadingCall":
+      return handleLeadingCall(state, action.skip);
+    case "trailingCall":
+      return handleTrailingCall(state, action.skip);
+    case "fulfill":
+      return handleFulfill(state, action.result);
+    case "reject":
+      return handleReject(state);
+    case "cancel":
+      return handleCancel(state);
+    case "reset":
+      return handleReset(state, action.result);
+  }
+};
+
 /**
  * useDebouncedAsyncCall debounces asynchronous function calls.
  * When the given function is invoked after timeout and it is fulfilled, the result will be set to
@@ -112,36 +174,103 @@ export type UseDebouncedAsyncCallResult<R, T extends readonly unknown[]> = [
 export function useDebouncedAsyncCall<R, T extends readonly unknown[]>(
   options: UseDebouncedAsyncCallOptions<R, T>
 ): UseDebouncedAsyncCallResult<R, T> {
-  const [result, call, isWaitingCall, { cancel: cancelCall, reset: resetCall }] = useAsyncCall({
-    func: options.func,
-    init: options.init,
-  });
-  const [
-    ,
-    debouncedCall,
-    isWaitingDebounce,
-    { cancel: cancelDebounce, reset: resetDebounce, flush },
-  ] = useDebouncedCall({
-    func: call,
-    init: undefined,
+  const leadingRef = useRef(options.leading ?? false);
+  const trailingRef = useRef(options.trailing ?? true);
+
+  const [state, dispatch] = useReducer<Reducer<State<R>, Action<R>>, R | (() => R)>(
+    reducer,
+    options.init,
+    initializer
+  );
+
+  const cancelAsyncCallRef = useRef<CancelFunc | undefined>(undefined);
+  const call = useCallback(
+    (args: T): void => {
+      if (cancelAsyncCallRef.current) {
+        const cancelAsyncCall = cancelAsyncCallRef.current;
+        cancelAsyncCall();
+        cancelAsyncCallRef.current = undefined;
+      }
+      const func = options.func;
+      [cancelAsyncCallRef.current] = attachActions(
+        func(...args),
+        result => {
+          cancelAsyncCallRef.current = undefined;
+          dispatch({ type: "fulfill", result });
+        },
+        err => {
+          // eslint-disable-next-line no-console
+          console.error(err);
+          cancelAsyncCallRef.current = undefined;
+          dispatch({ type: "reject" });
+        }
+      );
+    },
+    [options.func]
+  );
+
+  const { trigger: debouncedCall, cancel, flush } = useDebouncedPrim<T>({
+    leadingCallback: useCallback(
+      args => {
+        dispatch({ type: "start" });
+        if (leadingRef.current) {
+          dispatch({ type: "leadingCall", skip: false });
+          call(args);
+        } else {
+          dispatch({ type: "leadingCall", skip: true });
+        }
+      },
+      [call]
+    ),
+    trailingCallback: useCallback(
+      (args, count) => {
+        if (trailingRef.current && !(leadingRef.current && count === 1)) {
+          dispatch({ type: "trailingCall", skip: false });
+          call(args);
+        } else {
+          dispatch({ type: "trailingCall", skip: true });
+        }
+      },
+      [call]
+    ),
+    cancelCallback: useCallback(() => {
+      if (cancelAsyncCallRef.current) {
+        const cancelAsyncCall = cancelAsyncCallRef.current;
+        cancelAsyncCall();
+        cancelAsyncCallRef.current = undefined;
+      }
+      dispatch({ type: "cancel" });
+    }, []),
     wait: options.wait,
     maxWait: options.maxWait,
-    leading: options.leading,
-    trailing: options.trailing,
   });
-  // NOTE: cancel / reset functions are all stable
-  const cancelRef = useRef((): void => {
-    cancelDebounce();
-    cancelCall();
-  });
+
   const resetRef = useRef((result: R): void => {
-    resetDebounce();
-    resetCall(result);
+    cancel();
+    dispatch({ type: "reset", result });
   });
+
+  useEffect(
+    () => () => {
+      if (cancelAsyncCallRef.current) {
+        const cancelAsyncCall = cancelAsyncCallRef.current;
+        cancelAsyncCall();
+        cancelAsyncCallRef.current = undefined;
+      }
+    },
+    []
+  );
+
+  const result = state.result;
+  const isWaiting = state.type !== "standby"; // actually isWaitingOrPending
   return [
     result,
     debouncedCall,
-    isWaitingDebounce || isWaitingCall,
-    { cancel: cancelRef.current, reset: resetRef.current, flush },
+    isWaiting,
+    {
+      cancel,
+      reset: resetRef.current,
+      flush,
+    },
   ];
 }
