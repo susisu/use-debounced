479bf12cc41808c683c399f3eaf699e799d233e1
Susisu <susisu2413@yahoo.co.jp>
2024-02-18 14:43:15 +0900

    chore: lint tsx files

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import { config } from "@susisu/eslint-config";
+import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
@@ -6,24 +7,31 @@ import globals from "globals";
 export default config({}, [
   {
     plugins: {
+      react: reactPlugin,
       "react-hooks": reactHooksPlugin,
       vitest: vitestPlugin,
     },
   },
   {
-    files: ["src/**/*.ts"],
+    files: ["src/**/*.{ts,tsx}"],
     languageOptions: {
       globals: {
         ...globals.es2021,
         ...globals.browser,
       },
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
+      ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
     },
   },
   {
-    files: ["src/**/*.test.ts", "src/**/__tests__/**/*.ts"],
+    files: ["src/**/*.test.{ts,tsx}", "src/**/__tests__/**/*.{ts,tsx}"],
     rules: {
       ...vitestPlugin.configs.recommended.rules,
     },
